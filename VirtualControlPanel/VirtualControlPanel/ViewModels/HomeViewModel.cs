5d8edd2d73b10e512b37eee541fc0bf57271da71
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-05-04 00:38:59 +0200

    Add using

--- VirtualControlPanel/VirtualControlPanel/ViewModels/HomeViewModel.cs
+++ VirtualControlPanel/VirtualControlPanel/ViewModels/HomeViewModel.cs
@@ -7,6 +7,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using HanumanInstitute.MvvmDialogs;
 using VirtualControlPanel.Models;
+using VirtualControlPanel.Services;
 
 namespace VirtualControlPanel.ViewModels;
 
