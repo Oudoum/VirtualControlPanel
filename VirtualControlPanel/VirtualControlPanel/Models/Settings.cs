4abe63af1a31e05a075bbd4e6e42a817e5a5a63f
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-05-04 20:51:34 +0200

    Add position and size restore

--- VirtualControlPanel/VirtualControlPanel/Models/Settings.cs
+++ VirtualControlPanel/VirtualControlPanel/Models/Settings.cs
@@ -1,13 +1,23 @@
+using Avalonia.Controls;
+
 namespace VirtualControlPanel.Models;
 
 public class Settings
 {
     public string? IpAddress { get; set; } = "127.0.0.1";
     public int? Port { get; set; } = 2024;
-    
+
     public bool IsAutoStart { get; set; }
-    
+
     public bool IsCduLeftEnabled { get; set; }
     public bool IsCduRightEnabled { get; set; }
     public bool IsCduCenterEnabled { get; set; }
+
+    public WindowState WindowState { get; set; }
+
+    public int PositionX { get; set; }
+    public int PositionY { get; set; }
+
+    public double Width { get; set; }
+    public double Height { get; set; }
 }
