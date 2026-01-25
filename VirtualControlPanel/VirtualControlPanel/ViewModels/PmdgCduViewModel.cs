37dc90591375c56491477eab4fa9a1eb62ef69c1
Oudoum <77352752+Oudoum@users.noreply.github.com>
2026-01-25 02:02:55 +0100

    Fix blackscreen

--- VirtualControlPanel/VirtualControlPanel/ViewModels/PmdgCduViewModel.cs
+++ VirtualControlPanel/VirtualControlPanel/ViewModels/PmdgCduViewModel.cs
@@ -1,13 +1,31 @@
+using System;
 using CommunityToolkit.Mvvm.ComponentModel;
 using VirtualControlPanel.Models;
 using VirtualControlPanel.Services;
 
 namespace VirtualControlPanel.ViewModels;
 
-public class PmdgCduViewModel(string? title, CduSettings? cduSettings, SignalRClientService signalRClientService) : ObservableObject
+public class PmdgCduViewModel : ObservableObject
 {
-    public string? Title { get; private set; } = title;
-    public CduSettings CduSettings { get; private set; } = cduSettings ?? new CduSettings();
+    public string? Title { get; }
+    public CduSettings CduSettings { get; private set; }
+    public byte[] CduScreenData { get; private set; } = [];
 
-    public SignalRClientService SignalRClientService { get; private set; } = signalRClientService;
+    public event Action? ScreenUpdated;
+
+    public PmdgCduViewModel(string? title, CduSettings? cduSettings, SignalRClientService signalRClientService)
+    {
+        Title = title;
+        CduSettings = cduSettings ?? new CduSettings();
+        signalRClientService.PmdgDataReceived += (location, cduScreenData) =>
+        {
+            if (location != Title)
+            {
+                return;
+            }
+
+            CduScreenData = cduScreenData;
+            ScreenUpdated?.Invoke();
+        };
+    }
 }
