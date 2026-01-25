cf88d3f348c60284d6339ce85dafcabbead993e3
Oudoum <77352752+Oudoum@users.noreply.github.com>
2026-01-25 01:57:11 +0100

    Cleanup

--- VirtualControlPanel/VirtualControlPanel/Models/Cdu.cs
+++ VirtualControlPanel/VirtualControlPanel/Models/Cdu.cs
@@ -6,13 +6,13 @@ namespace VirtualControlPanel.Models;
 public static class Cdu
 {
     private const string Name = "CDU";
-    
+
     private const string Left = Name + "Left";
     private const string Right = Name + "Right";
     private const string Center = Name + "Center";
 
     public static string[] Locations  => [Left, Right, Center];
-    
+
     // CDU Screen Cell Structure
     //
 
@@ -20,20 +20,20 @@ public static class Cdu
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
     public struct Screen
     {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 24)]
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = CduColumns)]
         public Row[] Columns;
 
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
         public struct Row
         {
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 14)]
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = CduRows)]
             public Cell[] Rows;
 
             [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
             public struct Cell
             {
                 [MarshalAs(UnmanagedType.U1)]
-                public byte Symbol;
+                public char Symbol;
                 public Color Color; // any of CDU_COLOR_ defines
                 public Flags Flags;  // a combination of CDU_FLAG_ bits
             }
@@ -42,23 +42,11 @@ public static class Cdu
         [MarshalAs(UnmanagedType.I1)]
         public bool Powered; // true if CDU is powered
     }
-    
+
     private const int CduColumns = 24;
     private const int CduRows = 14;
     public const int CduCells = CduColumns * CduRows;
     public const int ScreenStateSize = CduCells * 3 + 1; // 3 = Symbol + Color + Flags | 1 = Powered
-
-    public struct ScreenBytes
-    {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = ScreenStateSize)]
-        public byte[] Data;
-    }
-    
-    public struct CduData(int id, Screen screen)
-    {
-        public int Id = id;
-        public Screen Screen = screen;
-    }
 
     // CDU Screen Cell Colors
     public enum Color : byte
@@ -80,7 +68,7 @@ public static class Cdu
         Reverse = 0x02,   // character background is highlighted in reverse video
         Unused = 0x04     // dimmed character color indicating inop/unused entries
     }
-    
+
     public enum DataRequestId
     {
         Cdu0 = 1,
